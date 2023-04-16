6a14cdd7f2c466b9521119abea28bc454aa76e1d
asliya2001 <asliyaabdulloeva@gmail.com>
2023-04-16 13:53:28 +0200

    Employee registration ui part added

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,2 +1,23 @@
-package com.cydeo.model;public class Employee {
+package com.cydeo.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
 }
